99b41ea09d52df22c3e91f34261dee1d69b16ed5
EnriqueG36 <112781278+EnriqueG36@users.noreply.github.com>
2022-09-14 14:15:13 -0500

    Add files via upload

---
+++ Desafio4/index.js
@@ -0,0 +1,31 @@
+
+
+// Intanciación del servidor-------------------------------------------------------------------------
+const express = require('express');         //Importamos express
+const apiRoutes = require('./routers/routers');
+
+const PORT = process.env.PORT || 8080;      //Definimos el puerto
+
+const app = express();
+
+// Middlewares---------------------------------------------------------------------------------------
+
+app.use(express.json());
+
+//Routes --------------------------------------------------------------------------------------------
+app.use('/api', apiRoutes);                 //Ruta a routers.js con prefijo /api
+
+
+
+//Ejecución del metodo listen------------------------------------------------------------------------
+
+//Log que nos indica que el servidor está corriendo
+const connectedServer = app.listen(PORT, () => {
+    console.log (`El servidor está listo y corriendo en el puerto ${PORT}`);
+})
+
+//En caso de erro, mostrar 
+connectedServer.on('error',(error) => {
+    console.log(error.message);
+})
+
